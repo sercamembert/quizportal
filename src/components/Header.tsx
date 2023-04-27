e6ce747196f8c839ea49fe43d0d1d40fc44fd920
Filip Maszota <filipmaszota3@gmail.com>
2023-04-27 17:52:29 +0200

    remove generate from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import { signInWithPopup } from "firebase/auth";
 export const Header = () => {
   const [user]: any = useAuthState(auth);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isPlusOpen, setIsPlusOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const signUserOut = async () => {
@@ -24,14 +24,18 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="header__logo-container">
-        <img src={logo} alt="logo" className="logo" tabIndex={0} />
+        <Link to="/">
+          {" "}
+          <img src={logo} alt="logo" className="logo" tabIndex={0} />
+        </Link>
       </div>
       <nav className="header__links">
         <Link
           to="/"
-          className={user ? "header__link" : "header__link--mobile"}
+          className={
+            user ? "header__link" : " header__link header__link--mobile"
+          }
           onClick={() => {
-            setIsPlusOpen(false);
             setIsDropdownOpen(false);
           }}
         >
@@ -41,7 +45,6 @@ export const Header = () => {
           to="/"
           className="header__link header__link--mobile"
           onClick={() => {
-            setIsPlusOpen(false);
             setIsDropdownOpen(false);
           }}
         >
@@ -51,58 +54,30 @@ export const Header = () => {
           to="/create"
           className="header__link header__link--media-display header__link--background"
           onClick={() => {
-            setIsPlusOpen(false);
             setIsDropdownOpen(false);
           }}
         >
           Create
         </Link>
-        <Link
-          to="/generate"
-          className="header__link header__link--media-display header__link--background"
-          onClick={() => {
-            setIsPlusOpen(false);
-            setIsDropdownOpen(false);
-          }}
-        >
-          Generate
-        </Link>
-        <div
-          className="header__plus"
-          tabIndex={0}
-          onClick={() => {
-            setIsPlusOpen(!isPlusOpen);
-            isDropdownOpen && setIsDropdownOpen(false);
-          }}
-        >
-          +
-        </div>
-        {isPlusOpen && (
-          <div className="dropdown dropdown--plus">
-            <div className="dropdown__link-container dropdown__link-container--border">
-              <Link
-                to="/create"
-                className="dropdown__link"
-                onClick={() => setIsPlusOpen(!isPlusOpen)}
-              >
-                <i className="fa-solid fa-pencil dropdown__icon"></i>
-                Create
-              </Link>
-            </div>
-            <div className="dropdown__link-container">
-              <Link
-                to="/generate"
-                className="dropdown__link"
-                onClick={() => setIsPlusOpen(!isPlusOpen)}
-              >
-                <i className="fa-solid fa-shuffle dropdown__icon"></i>
-                Generate
-              </Link>
-            </div>
+        <Link to="/create">
+          <div
+            className="header__plus"
+            tabIndex={0}
+            onClick={() => {
+              isDropdownOpen && setIsDropdownOpen(false);
+            }}
+          >
+            +
           </div>
-        )}
+        </Link>
       </nav>
-      <div className="header__profile">
+      <div
+        className={
+          user
+            ? "header__profile"
+            : "header__profile header__profile--logout-media"
+        }
+      >
         {!user ? (
           <div className="header__signIn">
             <span className="header__login" onClick={signInWithGoogle}>
@@ -120,7 +95,6 @@ export const Header = () => {
               className="header__profile header__profile-main"
               onClick={() => {
                 setIsDropdownOpen(!isDropdownOpen);
-                isPlusOpen && setIsPlusOpen(false);
               }}
               tabIndex={0}
             />
@@ -149,16 +123,6 @@ export const Header = () => {
                   >
                     <i className="fa-solid fa-pencil dropdown__icon"></i>
                     Create
-                  </Link>
-                </div>
-                <div className="dropdown__link-container">
-                  <Link
-                    to="/generate"
-                    className="dropdown__link"
-                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                  >
-                    <i className="fa-solid fa-shuffle dropdown__icon"></i>
-                    Generate
                   </Link>
                 </div>
                 <div className="dropdown__link-container">
