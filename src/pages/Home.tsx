6d2e57cc4ba274e0bb4777f4799a2c855d07137e
Filip Maszota <filipmaszota3@gmail.com>
2023-04-27 19:35:35 +0200

    add main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,28 @@
-import { auth, provider } from "../config/firebase";
-import { signInWithPopup } from "firebase/auth";
-import mainImg from "../img/main.jpg";
-import phoneImg from "../img/phone.jpg";
-import svg from "../img/svg.svg";
-import svg2 from "../img/svg.svg";
+import arrowImg from "../img/home-blocks/arrow.png";
+import waveImg from "../img/home-blocks/wave.png";
+import circleImg from "../img/home-blocks/circle.png";
+import notebookImg from "../img/home-blocks/notebook.png";
 
 export const Home = () => {
-  const signInWithGoogle = async () => {
-    const result = await signInWithPopup(auth, provider);
-  };
-
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <img src={notebookImg} alt="notebookImg" className="notebook" />
+      <img src={arrowImg} alt="arrowImg" className="arrow" />
+      <img src={waveImg} alt="waveImg" className="wave" />
+      <img src={circleImg} alt="circleImg" className="circle" />
+      <div className="home__container">
+        <div className="home__heading">
+          <span>
+            Make it easy to learn big topics with flashcards and practice tests
+          </span>
+        </div>
+        <span className="home__text">
+          Join more than 60M+ worldwide using Quizportal flashcards, practice
+          tests and games to achive their goals in school, univeristy and
+          beyound.
+        </span>
+        <button className="home__btn">Get Started</button>
+      </div>
+    </div>
+  );
 };
