fa372821a01e713722f482f698c2e94e8b575843
Filip Maszota <filipmaszota3@gmail.com>
2023-04-27 16:58:30 +0200

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,75 +1,194 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { auth } from "../config/firebase";
+import { auth, provider } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import logo from "../img/logo.png";
+import { signInWithPopup } from "firebase/auth";
 export const Header = () => {
-  const [user] = useAuthState(auth);
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [user]: any = useAuthState(auth);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isPlusOpen, setIsPlusOpen] = useState(false);
   const navigate = useNavigate();
-  const handleImageClick = () => {
-    setShowDropdown(!showDropdown);
-  };
+
   const signUserOut = async () => {
     await signOut(auth);
     navigate("/");
   };
 
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+  };
+
   return (
-    <div className="header">
-      <span className="header__link">
-        <Link to={"/"} className="header__link">
-          HOME
+    <header className="header">
+      <div className="header__logo-container">
+        <img src={logo} alt="logo" className="logo" tabIndex={0} />
+      </div>
+      <nav className="header__links">
+        <Link
+          to="/"
+          className={user ? "header__link" : "header__link--mobile"}
+          onClick={() => {
+            setIsPlusOpen(false);
+            setIsDropdownOpen(false);
+          }}
+        >
+          Home
         </Link>
-      </span>
-      <span className="header__link">
-        <Link to={"/create"} className="header__link">
-          CREATE
+        <Link
+          to="/"
+          className="header__link header__link--mobile"
+          onClick={() => {
+            setIsPlusOpen(false);
+            setIsDropdownOpen(false);
+          }}
+        >
+          FAQ
         </Link>
-      </span>
-      <span className="header__link">
-        <Link to={"/generate"} className="header__link">
-          GENERATE
+        <Link
+          to="/create"
+          className="header__link header__link--media-display header__link--background"
+          onClick={() => {
+            setIsPlusOpen(false);
+            setIsDropdownOpen(false);
+          }}
+        >
+          Create
         </Link>
-      </span>
-      <span className="header__link">
-        <Link to={"/flashcards"} className="header__link">
-          MY FLASHCARDS
+        <Link
+          to="/generate"
+          className="header__link header__link--media-display header__link--background"
+          onClick={() => {
+            setIsPlusOpen(false);
+            setIsDropdownOpen(false);
+          }}
+        >
+          Generate
         </Link>
-      </span>
-      <div className="header__img-container">
-        <img
-          src={user?.photoURL || ""}
-          className="header__img"
-          onClick={handleImageClick}
-        />
-        {showDropdown && (
-          <div className="header__dropdown">
-            <div className="header__user">
-              <span className="header__username">{user?.displayName}</span>
-            </div>
-            <span className="header__dropdown-link">
+        <div
+          className="header__plus"
+          tabIndex={0}
+          onClick={() => {
+            setIsPlusOpen(!isPlusOpen);
+            isDropdownOpen && setIsDropdownOpen(false);
+          }}
+        >
+          +
+        </div>
+        {isPlusOpen && (
+          <div className="dropdown dropdown--plus">
+            <div className="dropdown__link-container dropdown__link-container--border">
               <Link
-                to={"/flashcards"}
-                className="header__dropdown-Link"
-                onClick={handleImageClick}
+                to="/create"
+                className="dropdown__link"
+                onClick={() => setIsPlusOpen(!isPlusOpen)}
               >
-                MY FLASHCARDS
+                <i className="fa-solid fa-pencil dropdown__icon"></i>
+                Create
               </Link>
+            </div>
+            <div className="dropdown__link-container">
+              <Link
+                to="/generate"
+                className="dropdown__link"
+                onClick={() => setIsPlusOpen(!isPlusOpen)}
+              >
+                <i className="fa-solid fa-shuffle dropdown__icon"></i>
+                Generate
+              </Link>
+            </div>
+          </div>
+        )}
+      </nav>
+      <div className="header__profile">
+        {!user ? (
+          <div className="header__signIn">
+            <span className="header__login" onClick={signInWithGoogle}>
+              Login
             </span>
-            <span
-              className="header__dropdown-link"
+            <span className="header__signup" onClick={signInWithGoogle}>
+              Sign up
+            </span>
+          </div>
+        ) : (
+          <div className="header__profile-image-container">
+            <img
+              src={user?.photoURL ?? ""}
+              alt="user profile"
+              className="header__profile header__profile-main"
               onClick={() => {
-                signUserOut();
-                handleImageClick();
+                setIsDropdownOpen(!isDropdownOpen);
+                isPlusOpen && setIsPlusOpen(false);
               }}
-            >
-              LOGOUT
-            </span>
+              tabIndex={0}
+            />
+            {isDropdownOpen && (
+              <nav className="dropdown">
+                <div className="dropdown__profil">
+                  <div className="dropdown__img">
+                    <img
+                      src={user?.photoURL}
+                      alt="user profile"
+                      className="header__profile header__profile--dropdown"
+                    />
+                  </div>
+                  <div className="dropdown__user-details">
+                    <span className="dropdown__username">
+                      {user.displayName}
+                    </span>
+                    <span className="dropdown__email">{user.email}</span>
+                  </div>
+                </div>
+                <div className="dropdown__link-container dropdown__link-container--border">
+                  <Link
+                    to="/create"
+                    className="dropdown__link"
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                  >
+                    <i className="fa-solid fa-pencil dropdown__icon"></i>
+                    Create
+                  </Link>
+                </div>
+                <div className="dropdown__link-container">
+                  <Link
+                    to="/generate"
+                    className="dropdown__link"
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                  >
+                    <i className="fa-solid fa-shuffle dropdown__icon"></i>
+                    Generate
+                  </Link>
+                </div>
+                <div className="dropdown__link-container">
+                  <Link
+                    to="/flashcards"
+                    className="dropdown__link"
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                  >
+                    <i className="fa-solid fa-bookmark dropdown__icon"></i>
+                    My flashcards
+                  </Link>
+                </div>
+                <div className="dropdown__link-container dropdown__link-container--border">
+                  <span
+                    onClick={() => {
+                      setIsDropdownOpen(!isDropdownOpen);
+                      signUserOut();
+                    }}
+                    tabIndex={0}
+                    className="dropdown__link"
+                  >
+                    <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                    Log out
+                  </span>
+                </div>
+              </nav>
+            )}
           </div>
         )}
       </div>
-    </div>
+    </header>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import { auth, provider } from "../config/firebase";
 import { signInWithPopup } from "firebase/auth";
+import mainImg from "../img/main.jpg";
+import phoneImg from "../img/phone.jpg";
+import svg from "../img/svg.svg";
+import svg2 from "../img/svg.svg";
+
 export const Home = () => {
   const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider);
   };
-  return (
-    <div className="home">
-      <button onClick={signInWithGoogle}>Login with google</button>
-    </div>
-  );
+
+  return <div className="home"></div>;
 };
