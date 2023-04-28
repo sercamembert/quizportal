913898199775510ae7dded8b7eedb533166dda1b
Filip Maszota <filipmaszota3@gmail.com>
2023-04-28 16:45:36 +0200

    add scroll down arrow to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import arrowImg from "../img/home-blocks/arrow.png";
 import waveImg from "../img/home-blocks/wave.png";
 import circleImg from "../img/home-blocks/circle.png";
 import notebookImg from "../img/home-blocks/notebook.png";
-
+import downArrowImg from "../img/home-blocks/down-arrow.png";
 export const Home = () => {
   return (
     <div className="home">
@@ -12,7 +13,7 @@ export const Home = () => {
       <img src={circleImg} alt="circleImg" className="circle" />
       <div className="home__container">
         <div className="home__heading">
-          <span>
+          <span className="home__heading-text">
             Make it easy to learn big topics with flashcards and practice tests
           </span>
         </div>
@@ -22,6 +23,18 @@ export const Home = () => {
           beyound.
         </span>
         <button className="home__btn">Get Started</button>
+        <div className="home__scroll-container">
+          <span className="home__scroll-text">
+            90% of students approve that thanks to Quizportal{" "}
+            {window.innerWidth >= 1024 && <br />}
+            they started getting better grades
+          </span>
+          <img
+            src={downArrowImg}
+            alt="scroll down"
+            className="home__scroll-down"
+          />
+        </div>
       </div>
     </div>
   );
