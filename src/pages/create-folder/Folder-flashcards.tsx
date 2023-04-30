6f4a6d412ba1cc83f78e5289d40fcea4a6614c10
Filip Maszota <filipmaszota3@gmail.com>
2023-04-30 17:09:58 +0200

    add removing folder functionality

--- src/pages/create-folder/Folder-flashcards.tsx
+++ src/pages/create-folder/Folder-flashcards.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { collection, query, getDocs } from "firebase/firestore";
+import { collection, query, getDocs, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../../config/firebase";
-
+import { useNavigate } from "react-router-dom";
 interface FlashcardI {
   frontSite: string;
   backSite: string;
@@ -17,6 +17,8 @@ export const FolderFlashcards = () => {
   const { folderId } = useParams<FolderFlashcardsParams>();
   const [flashcards, setFlashcards] = useState<FlashcardI[]>([]);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     const fetchFlashcards = async () => {
       const q = query(collection(db, `Folders/${folderId}/Flashcards`));
@@ -29,6 +31,17 @@ export const FolderFlashcards = () => {
     fetchFlashcards();
   }, [folderId]);
 
+  const deleteFolder = async () => {
+    try {
+      if (folderId != null) {
+        const folderRef = doc(db, "Folders", folderId);
+        await deleteDoc(folderRef);
+      }
+    } catch (error) {
+      console.error("Błąd podczas usuwania dokumentu: ", error);
+    }
+  };
+
   return (
     <div>
       {flashcards.map((flashcard, index) => (
@@ -38,6 +51,14 @@ export const FolderFlashcards = () => {
           <p>Back: {flashcard.backSite}</p>
         </div>
       ))}
+      <button
+        onClick={() => {
+          deleteFolder();
+          navigate("/");
+        }}
+      >
+        delete folder
+      </button>
     </div>
   );
 };
