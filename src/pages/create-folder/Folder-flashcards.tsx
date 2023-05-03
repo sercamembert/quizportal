8cbfdc8b539bae709a72df6e18a4e667f29a9b66
Filip Maszota <filipmaszota3@gmail.com>
2023-05-03 11:33:25 +0200

    add displaying flashcards page

--- src/pages/create-folder/Folder-flashcards.tsx
+++ src/pages/create-folder/Folder-flashcards.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import {
   collection,
@@ -13,6 +13,7 @@ import {
 import { auth, db } from "../../config/firebase";
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { UserNotLogged } from "../../components/User-not-logged";
 
 interface FlashcardI {
   frontSite: string;
@@ -36,6 +37,12 @@ export const FolderFlashcards = () => {
   const [folder, setFolder] = useState<FolderI | null>(null);
   const [user]: any = useAuthState(auth);
   const navigate = useNavigate();
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlippedHorizontal, setIsFlippedHorizontal] = useState(false);
+  const [isActionsShowed, setIsActionsShowed] = useState(false);
+  const [isTermsShow, serIsTermsShow] = useState(true);
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchFlashcards = async () => {
@@ -57,6 +64,22 @@ export const FolderFlashcards = () => {
       }
     };
     fetchFolder();
+
+    function handleClickOutside(event: any) {
+      if (
+        event.target.closest(".share-modal__content") === null &&
+        event.target.closest(".flashcards__buttons") === null
+      ) {
+        setIsShareModalOpen(false);
+        setIsActionsShowed(false);
+      }
+    }
+
+    window.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("mousedown", handleClickOutside);
+    };
   }, [folderId]);
 
   const deleteFolder = async () => {
@@ -84,17 +107,158 @@ export const FolderFlashcards = () => {
     }
   };
 
+  const handleNextCard = () => {
+    setIsFlipped(false);
+    setIsFlippedHorizontal(true);
+
+    setTimeout(() => {
+      setIsFlippedHorizontal(false);
+      if (currentCardIndex < flashcards.length - 1) {
+        setCurrentCardIndex(currentCardIndex + 1);
+      } else {
+        setCurrentCardIndex(0);
+      }
+    }, 500);
+  };
+
+  const handlePrevCard = () => {
+    setIsFlipped(false);
+    setIsFlippedHorizontal(true);
+    setTimeout(() => {
+      setIsFlippedHorizontal(false);
+      if (currentCardIndex > 0) {
+        setCurrentCardIndex(currentCardIndex - 1);
+      } else {
+        setCurrentCardIndex(flashcards.length - 1);
+      }
+    }, 500);
+  };
+
+  const handleCardClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const handleShareClick = () => {
+    setIsShareModalOpen(!isShareModalOpen);
+  };
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(
+      `${window.location.origin}/folder-flashcards/${folderId}`
+    );
+  };
+
   return (
-    <div>
-      {flashcards.map((flashcard, index) => (
-        <div key={index}>
-          <h2>Flashcard {index + 1}</h2>
-          <p>Front: {flashcard.frontSite}</p>
-          <p>Back: {flashcard.backSite}</p>
+    <div className="flashcards">
+      {user ? (
+        <div className="flashcards__wrapper">
+          <span className="flashcards__title">{folder?.title}</span>
+          <div
+            className={`flashcards__card ${isFlipped ? "flipped" : ""} ${
+              isFlippedHorizontal && "flipped-horizontal"
+            }`}
+            onClick={handleCardClick}
+          >
+            <div className="front">
+              <p>{flashcards[currentCardIndex]?.frontSite}</p>
+            </div>
+            <div
+              className="back"
+              style={{ opacity: !isFlipped ? 0 : undefined }}
+            >
+              <p>{flashcards[currentCardIndex]?.backSite}</p>
+            </div>
+          </div>
+          <div className="flashcards__buttons">
+            <div className="flashcards__arrows">
+              <button onClick={handlePrevCard} className="flashcards__arrow">
+                <i className="fa-solid fa-arrow-left"></i>
+              </button>
+              <span className="flashcards__card-number">
+                {currentCardIndex + 1}/{flashcards.length}
+              </span>
+              <button onClick={handleNextCard} className="flashcards__arrow">
+                <i className="fa-solid fa-arrow-right"></i>
+              </button>
+            </div>
+            <div className="flashcards__actions-container">
+              <div className="flashcards__actions">
+                {isActionsShowed && (
+                  <>
+                    <div className="flashcards__action">
+                      <i className="fa-regular fa-pen-to-square"></i>
+                      <span>Edit</span>
+                    </div>
+                    <div
+                      className="flashcards__action"
+                      onClick={handleShareClick}
+                    >
+                      <i className="fa-regular fa-share-from-square"></i>
+                      <span>Share</span>
+                    </div>
+                    {folder?.userId === user?.uid && (
+                      <div
+                        className="flashcards__action"
+                        onClick={deleteFolder}
+                      >
+                        <i className="fa-regular fa-trash-can"></i>
+                        <span className="flashcards__action-text">Delete</span>
+                      </div>
+                    )}
+                  </>
+                )}
+              </div>
+              <div className="flashcards__show-actions">
+                <i
+                  className="fa-solid fa-gear"
+                  onClick={() => setIsActionsShowed(!isActionsShowed)}
+                ></i>
+              </div>
+            </div>
+          </div>
+          <div className="flashcards__terms">
+            <div className="flashcards__terms-heading">
+              <span className="flashcards__terms-title">Terms in set</span>
+              <span
+                className="flashcards__terms-show-hide"
+                onClick={() => serIsTermsShow(!isTermsShow)}
+              >
+                {isTermsShow ? "Hide" : "Show"}
+              </span>
+            </div>
+            {isTermsShow && (
+              <div className="flashcards__terms-container">
+                {flashcards.map((term) => {
+                  return (
+                    <div className="flashcards__terms-item">
+                      <span className="flashcards__term">{term.frontSite}</span>
+                      <span className="flashcards__meaning">
+                        {term.backSite}
+                      </span>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
         </div>
-      ))}
-      {folder?.userId === user?.uid && (
-        <button onClick={deleteFolder}>delete folder</button>
+      ) : (
+        <UserNotLogged />
+      )}
+      {isShareModalOpen && (
+        <div className="share-modal">
+          <div className="share-modal__content">
+            <div className="share-modal__link">{`${window.location.origin}/folder-flashcards/${folderId}`}</div>
+            <button
+              className="share-modal__copy"
+              onClick={() => {
+                handleCopyClick();
+                setIsShareModalOpen(false);
+              }}
+            >
+              Copy Link
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
